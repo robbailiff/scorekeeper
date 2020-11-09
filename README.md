3309c42f5e868abe4b793e9926a24703c36e6ff4
robbailiff <robbailiff@gmail.com>
2020-11-09 20:01:42 +0000

    Commit of the Ping Pong Score Keeper Assisgnment for the Web Development Bootcamp

--- README.md
+++ README.md
@@ -1,2 +1,17 @@
-# scorekeeper
-A simple widget made in Javascript to keep the score in a game of ping pong
+# Scorekeeper Exercise
+
+## Introduction
+
+For this assignment, the aim was to create a score keeper widget for a game of ping pong. It should allow you to set the number of wins required, use buttons to increase the wins for a player, indicate when one player has won the required number of games and then chnage the score colours and disable the buttons, and then finally a buttom to reset the scores. We had to do the logic using Javascript and do the structure and styling using the **Bulma** CSS framework.
+
+Check out the Bulma documentation here: https://bulma.io/documentation/
+
+Here is a link to the live web version: https://robbailiff.github.io/scorekeeper/
+
+The assignment is part of the **Web Developer Bootcamp 2020** on Udemy by **Colt Steele**. This is an in-depth course with over 60 hours of content covering HTML, CSS, Javascript, Node, Express, Mongo DB and more.
+
+You can find the Web Developer Bootcamp 2020 here: https://www.udemy.com/course/the-web-developer-bootcamp/
+
+## Comments
+
+This was quite a tough project and an excellent learning experience that took me the best part of a day to complete. I found the Javascript logic not too bad to work out and get functioning quite quickly, but I did spend a bit of time reorganizing the code and making it cleaner. I really liked the challenge of having to work with a new CSS framework in Bulma, which I have never used before, and work out from the documentation its syntax and how it works. I liked it and found that compared to Bootstrap it was more lightweight and the syntax was much more intuitive. The documentation was decent but wasn't anywhere near as in-depth or have as many features and examples as Bootstrap.
---
+++ score.js
@@ -0,0 +1,84 @@
+// Display selectors
+const p1Text = document.querySelector("#p1Display");
+const p2Text = document.querySelector("#p2Display");
+
+// Game number dropdown selector
+const gameNumber = document.querySelector("#gameNumber");
+
+// Score and winner variables
+let gameWon = false;
+let p1Score = 0;
+let p2Score = 0;
+
+// Function to update scores
+function newScore (p1Value, p2Value) {
+    p1Score = p1Value;
+    p2Score = p2Value;
+    // Change display
+    p1Text.textContent = p1Value;
+    p2Text.textContent = p2Value;
+}
+
+// Function to check for a winner
+function checkWin () {
+    if (p1Score >= Number(gameNumber.value)) {
+        addStyle (p1Text, p2Text);
+        gameWon = true;
+
+    } else if (p2Score >= Number(gameNumber.value)) {
+        addStyle (p2Text, p1Text);
+        gameWon = true;
+
+    }
+}
+
+// Function to change styling on a win
+function addStyle (winner, loser) {
+    // Turn p1Score green
+    winner.classList.add('has-text-success');
+    // Turn p2Score red
+    loser.classList.add('has-text-danger');
+    // Disable add buttons
+    p1.disabled = true;
+    p2.disabled = true;
+}
+
+// Function to remove styling on a reset
+function removeStyle (winner, loser) {
+    winner.classList.remove('has-text-success');
+    loser.classList.remove('has-text-danger');
+    p1.disabled = false;
+    p2.disabled = false;
+}
+
+// Bottom button selectors
+const p1 = document.querySelector("#p1Button");
+const p2 = document.querySelector("#p2Button");
+const reset = document.querySelector("#reset");
+
+// Player 1 button function
+p1.addEventListener('click', function () {
+    p1Score++;
+    newScore(p1Score, p2Score);
+    checkWin();
+})
+
+// Player 2 button function
+p2.addEventListener('click', function () {
+    p2Score++;
+    newScore(p1Score, p2Score);
+    checkWin();
+})
+
+// Reset button function
+reset.addEventListener('click', function (){
+    if (gameWon && p1Score >= Number(gameNumber.value)) {
+        removeStyle (p1Text, p2Text);
+        gameWon = false;
+
+    } else if (gameWon && p2Score >= Number(gameNumber.value)) {
+        removeStyle (p2Text, p1Text);
+        gameWon = false;
+    }
+    newScore(0, 0);
+})
